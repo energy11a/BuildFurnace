2b863ad55147fb1f23b9b883b5d1e30761556b8c
energy11a <kaurlohmus8@gmail.com>
2025-10-23 16:18:12 +0300

    buggy aga kuidagi töötav 3d ehitus

--- Assets/Scripts/BuildingSystem.cs
+++ Assets/Scripts/BuildingSystem.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using UnityEngine.UI;
@@ -14,6 +15,9 @@ public class BuildingSystem: MonoBehaviour
     public GameObject prefab2;
 
     private PlaceableObject _objectToPlace;
+
+    // Hoia koik grid positsioonid klotside jaoks
+    private Dictionary<Vector3Int, GameObject> placedBlocks = new Dictionary<Vector3Int, GameObject>();
 
     #region Unity methods
 
@@ -48,20 +52,52 @@ public class BuildingSystem: MonoBehaviour
             if (hit.collider.CompareTag("BuildPlatform") || hit.collider.CompareTag("Cube"))
             {
                 Vector3Int cellPosition = gridLayout.WorldToCell(hit.point);
-                position = gridLayout.CellToWorld(cellPosition) + gridLayout.cellSize / 2f;
-                Debug.Log(position.ToString());
+
+                // Arvuta hiire positsioon kohaliku ruumi suhtes
+                Vector3 localHit = hit.collider.transform.InverseTransformPoint(hit.point);
+                Vector3Int offset = Vector3Int.zero;
+
+                // Snap X/Z kulgedele
+                if (Mathf.Abs(localHit.x) > Mathf.Abs(localHit.z))
+                    offset.x = (localHit.x > 0) ? 1 : -0;
+                else
+                    offset.z = (localHit.z > 0) ? 1 : -0;
+
+                // Snap peale Y, kui hiir on klotsi kohal
+                if (localHit.y > 0.1f)
+                    offset.y = 1;
+
+                Vector3Int newCell = cellPosition + offset;
+
+                // Kui uus lahter juba tais, tosta Y +1
+                if (placedBlocks.ContainsKey(newCell))
+                    newCell.y += 1;
+
+                // X ja Z suunal liigutame ainult 1 grid vorra, mitte pool cellSize
+                Vector3 basePos = gridLayout.CellToWorld(cellPosition);
+                position.x = basePos.x + offset.x * gridLayout.cellSize.x;
+                position.z = basePos.z + offset.z * gridLayout.cellSize.z;
+
+                // Y positsioon uhe grid sammuga ules (voi pool cellSize, kui alustame pinnast)
+                position.y = basePos.y + offset.y * gridLayout.cellSize.y;
             }
             else
             {
                 position = hit.point;
             }
         }
-        position.y = transform.position.y+ gridLayout.cellSize.y / 2f;
+        else
+        {
+            position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f));
+        }
+
         return position;
     }
 
+
+
     #endregion
-    
+
     #region Building placement
 
     public void InitializeWithObject(GameObject prefab)

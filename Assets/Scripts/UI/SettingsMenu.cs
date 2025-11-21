c33890c08ff81463032f847ff771358277173810
Anton Otrokov <anton.otroko@gmail.com>
2025-11-21 20:13:26 +0200

    Frontend of Settings Tabs

--- Assets/Scripts/UI/SettingsMenu.cs
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -4,11 +4,24 @@ using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
+
     [Header("UI")]
     [SerializeField] private GameObject pauseMenu;
     [SerializeField] private GameObject settingsMenu;
     [SerializeField] private Button settingsButton;
+
+    [Header("Tabs")]
+    [SerializeField] private GameObject audioTab;
+    [SerializeField] private GameObject graphicsTab;
+    [SerializeField] private GameObject controlsTab;
+
+    [Header("TabButton")]
+    [SerializeField] private Button audioBtn;
+    [SerializeField] private Button graphicsBtn;
+    [SerializeField] private Button controlsBtn;
+
     [Header("UI Sliders")]
+    [SerializeField] private Slider generalSlider;
     [SerializeField] private Slider musicSlider;
     [SerializeField] private Slider sfxSlider;
 

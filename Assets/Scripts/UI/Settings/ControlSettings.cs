dc4c95de140e114783be01d26a97ff17e44289de
Anton Otrokov <anton.otroko@gmail.com>
2025-11-23 12:27:15 +0200

    basis of keymapping

--- Assets/Scripts/UI/Settings/ControlSettings.cs
+++ Assets/Scripts/UI/Settings/ControlSettings.cs
@@ -1,10 +1,20 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class ControlSettings : MonoBehaviour
 {
-    // Connect buttons in tha editor
+    [SerializeField] Button camRightBtn;
+    [SerializeField] Button camLeftBtn;
+    [SerializeField] Button startSimBtn;
+    [SerializeField] Button pauseBtn;
 
 
+
+    private void Start()
+    {
+        Debug.Log(InputSystem.actions.actionMaps[2]);  
+    }
 
 }
